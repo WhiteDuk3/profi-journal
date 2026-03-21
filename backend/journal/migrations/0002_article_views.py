7ba58181b9e0cf8513af83dea1926ce384c42fcb
WhiteDuk3 <i.mahmudov@newuu.uz>
2026-03-21 12:31:45 +0500

    Create 0002_article_views.py

---
+++ backend/journal/migrations/0002_article_views.py
@@ -0,0 +1,16 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('journal', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='article',
+            name='views',
+            field=models.IntegerField(default=0),
+        ),
+    ]
